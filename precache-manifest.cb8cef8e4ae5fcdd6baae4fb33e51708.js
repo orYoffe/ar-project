814108630ac6aed8029bedcb39a9d6b841238680
orYoffe <ragiop07@gmail.com>
2019-01-03 15:09:13 +0100

    Updates

---
+++ precache-manifest.cb8cef8e4ae5fcdd6baae4fb33e51708.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d921df279aaa2122aa2a",
+    "url": "/ar-project/static/js/runtime~main.d921df27.js"
+  },
+  {
+    "revision": "3125d3949f4df58a6bd7",
+    "url": "/ar-project/static/js/main.3125d394.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/ar-project/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "3125d3949f4df58a6bd7",
+    "url": "/ar-project/static/css/main.2a615f3c.chunk.css"
+  },
+  {
+    "revision": "8ee7b540d8828bf7659efdc68bca8c11",
+    "url": "/ar-project/index.html"
+  }
+];
