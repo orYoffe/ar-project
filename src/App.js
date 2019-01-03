185807319ce4361f692d3d9db8d8d58d84c6eef4
orYoffe <ragiop07@gmail.com>
2019-01-03 15:22:55 +0100

    added instructions

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-return <div/>;
+return <p style={{
+  position: 'absolute',
+  bottom: '10px',
+  width: '100%',
+  textAlign: 'center',
+  background: 'rgba(255, 255, 255, 0.2)'
+}}>
+show the Hiro image to the camera
+</p>;
   }
 }
 
